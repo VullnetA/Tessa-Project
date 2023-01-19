ba0baae402fd652bcf7f9934a43be2f95ddc0b3b
Nazif <116115695+ns29248@users.noreply.github.com>
2023-01-19 03:57:55 +0100

    Implementing Edit(Update) and Delete Products to the Admin Panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,6 +64,54 @@ class AdminController extends Controller
 
     }
 
+    public function show_product(){
 
+        $product=Product::all();
+        return view('admin.show_product', compact('product'));
+    }
+
+    public function delete_product($id){
+        
+        $data=Product::find($id);
+        $data->delete();
+
+        return redirect()->back()->with('message', 'Product Deleted Successfully' );
+
+    }
+
+    public function update_product($id){
+
+        $product=Product::find($id);
+        $category=Category::all();
+
+        return view ('admin.update_product', compact('product', 'category'));
+    }
+
+    public function update_product_confirm(Request $request, $id){
+
+        $product=Product::find($id);
+
+        $product->title=$request->title;
+        $product->description=$request->description;
+        $product->category=$request->category;
+        $product->brand=$request->brand;
+        $product->quantity=$request->quantity;
+        $product->price=$request->price;
+        $product->discount_price=$request->discount_price;
+       
+        $image=$request->image;
+        if($image){
+
+
+            $imagename=time().'.'.$image->getClientOriginalExtension();
+            $request->image->move('Product', $imagename);
+            $product->image=$imagename;
+
+        }
+
+        $product->save();
+
+        return Redirect()->back()->with('message', 'Product Updated Successfully');
+    }
 }
 
